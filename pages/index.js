8ffa7a5d46527ad5a1cc8215aee8e11ea5fd883f
zhouruifa <zhouruifa@bytedance.com>
2023-10-08 11:53:01 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({allPostsData}) {
               <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-              <p>我是程江慧</p>
+              <p>我是周瑞发</p>
             </section>
           <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
               <h2 className={utilStyles.headingLg}>Blog</h2>
